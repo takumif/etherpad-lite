a715ca290053486d99586a6ca329653072ed7263
Pratik Prakash <pratikprakash1@yahoo.com>
2015-10-28 15:08:09 -0400

    Added basic loadtests and instructions to how to run them

---
+++ loadtests.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+#Author: Pratik Prakash
+
+echo "STARTING STRESS TESTS FOR ETHERPAD"
+echo "================================"
+echo "================================"
+
+#Run ETHERPAD FILE in a separate terminal window
+#./bin/run.sh
+
+
+# TEST NUM OF CONCURRENT USERS 
+#==============================
+
+# Tests 10 concurrent clients for 1000 requests
+echo "TESTING 10 CONCURRENT USERS"
+echo "TESTING 10 CONCURRENT USERS" > load_test_report.txt
+loadtest http://localhost:9001/ -t 20 -c 10 -k -n 1000 >> load_test_report.txt
+echo "================================"
+echo "================================"
+
+# Tests 100 concurrent clients for 1000 requests
+echo "TESTING 100 CONCURRENT USERS"
+echo "TESTING 100 CONCURRENT USERS" >> load_test_report.txt
+loadtest http://localhost:9001/ -t 20 -c 100 -k -n 1000 >> load_test_report.txt
+echo "================================"
+echo "================================"
+
+# Tests 200 concurrent clients for 1000 requests
+echo "TESTING 200 CONCURRENT USERS"
+echo "TESTING 200 CONCURRENT USERS" >> load_test_report.txt
+loadtest http://localhost:9001/ -t 20 -c 200 -k -n 1000 >> load_test_report.txt
+echo "================================"
+echo "================================"
+
+
+
+# TEST NUM OF REQUESTS with fixed users = 10 
+#=========================================
+
+# Tests 10 concurrent clients for 1000 requests
+echo "TESTING 1000 REQUESTS"
+echo "TESTING 1000 REQUESTS" >> load_test_report.txt
+loadtest http://localhost:9001/ -t 20 -c 10 -k -n 1000 >> load_test_report.txt
+echo "================================"
+echo "================================"
+
+# Tests 10 concurrent clients for 5000 requests
+echo "TESTING 5000 REQUESTS"
+echo "TESTING 5000 REQUESTS" >> load_test_report.txt
+loadtest http://localhost:9001/ -t 20 -c 10 -k -n 5000 >> load_test_report.txt
+echo "================================"
+echo "================================"
+
+# Tests 10 concurrent clients for 10000 requests
+echo "TESTING 5000 REQUESTS"
+echo "TESTING 5000 REQUESTS" >> load_test_report.txt
+loadtest http://localhost:9001/ -t 20 -c 10 -k -n 10000 >> load_test_report.txt
+echo "================================"
+echo "================================"
+
+
+
+# TEST REQUESTS PER SECOND with fixed users = 10 
+#===============================================
+
+# Tests 10 concurrent clients for 100 RPS
+echo "TESTING 100 REQUESTS PER SECOND"
+echo "TESTING 100 REQUESTS PER SECOND" >> load_test_report.txt
+loadtest http://localhost:9001/ -t 20 -c 10 -k --rps 100 >> load_test_report.txt
+echo "================================"
+echo "================================"
+
+# Tests 10 concurrent clients for 300 RPS
+echo "TESTING 300 REQUESTS PER SECOND"
+echo "TESTING 300 REQUESTS PER SECOND" >> load_test_report.txt
+loadtest http://localhost:9001/ -t 20 -c 10 -k --rps 300 >> load_test_report.txt
+echo "================================"
+echo "================================"
+
+# Tests 10 concurrent clients for 500 RPS
+echo "TESTING 500 REQUESTS PER SECOND"
+echo "TESTING 500 REQUESTS PER SECOND" >> load_test_report.txt
+loadtest http://localhost:9001/ -t 20 -c 10 -k --rps 500 >> load_test_report.txt
+echo "================================"
+echo "================================"
+
+echo "FINISHED STRESS TESTING ETHERPAD"
+echo "================================"
+echo "REPORT CAN BE FOUND IN CURRENT DIRECTORY IN A FILE CALLED load_test_report.txt"
